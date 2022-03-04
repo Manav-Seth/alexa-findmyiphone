35f67089459240a0bc309131726fb584b1f63ffd
Manav-Seth <manav.seth@hotmail.com>
2022-03-04 11:26:41 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ DIR=`pwd`
 
 rm -f package.zip
 
-zip package.zip app.py users.py
+zip package.zip lambda_function.py users.py
 
-cd venv/lib/python2.7/site-packages/
+cd venv/lib/python3.9/site-packages/
 zip -g -r9 $DIR/package.zip *
 
 cd $DIR
